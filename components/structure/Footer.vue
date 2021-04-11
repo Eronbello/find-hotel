fe66e44f8847db3f9172d88fe9a0605d908fa2b8
Eronbello <eron.bello@gmail.com>
2021-04-10 21:14:57 -0300

    feat: add footer

---
+++ components/structure/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="tui-footer">
+    <div class="tui-container">
+      <a
+        href="http://www.tuigroup.com/en-en"
+        target="_blank"
+        rel="noopener"
+        class="app-copyrights"
+        data-v-38e248be=""
+        data-v-a1d4f7a0=""
+        >© TUI Group</a
+      >
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.tui-footer {
+  display: flex;
+  padding: 32px 0;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid rgba(0, 0, 0, 0.2);
+
+  a {
+    text-decoration: none;
+    transition: fill 0.25s cubic-bezier(0, 0, 0.5, 0.5);
+    font-weight: 400;
+    font-style: normal;
+    font-size: 16px;
+    line-height: 24px;
+    color: #9b9b9b;
+    &:hover {
+      color: #10a0de;
+    }
+  }
+}
+</style>
