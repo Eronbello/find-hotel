13716c612981378e3d6e92047366c1b13c2e8243
Eronbello <eron.bello@gmail.com>
2021-04-11 16:34:15 -0300

    feat: add i18n

--- pages/search.vue
+++ pages/search.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="tui-search-page">
+    <Toaster
+      message="Please fill all the form"
+      color="#d40e14"
+      :show="showToaster"
+    />
     <div class="tui-container">
-      <div class="tui-search-bar">
-        <SearchCard />
+      <div v-if="!isLoading" class="tui-search-bar">
+        <SearchCard :form="form" @search="onSearch" />
       </div>
-      <div v-if="hotels && hotels.length === 0" class="tui-hotels-loading">
+      <div v-if="isLoading" class="tui-hotels-loading">
         <LoadingIcon />
       </div>
       <div v-else class="tui-hotels-list">
         <template v-for="hotel in hotels">
           <HotelSearchCard
+            v-if="existHotels"
             :key="hotel.hotelId"
             :hotel="hotel"
             @selected="openDetails"
           />
         </template>
+        <h3 v-if="!existHotels" class="tui-hotels-list__not-exists">
+          There are no hotels with these parameters
+        </h3>
       </div>
     </div>
   </div>
@@ -26,17 +35,23 @@ import apiService from '@/services/api'
 import SearchCard from '@/components/shared/SearchCard.vue'
 import HotelSearchCard from '@/components/shared/HotelSearchCard.vue'
 import LoadingIcon from '@/components/shared/LoadingIcon.vue'
+import Toaster from '@/components/shared/Toaster.vue'
 import search from '@/@types/search'
 export default Vue.extend({
-  components: { SearchCard, HotelSearchCard, LoadingIcon },
+  components: { SearchCard, HotelSearchCard, LoadingIcon, Toaster },
   data: () => ({
     hotels: [],
+    showToaster: false,
+    isLoading: true,
+    form: {},
   }),
+  computed: {
+    existHotels() {
+      return this.hotels.length > 0
+    },
+  },
   async mounted() {
-    const params = this.getParams()
-    const response = await apiService.search(params)
-    const data = await response.json()
-    this.hotels = data.data
+    await (this as any).startSearch()
   },
   methods: {
     getParams(): search {
@@ -45,6 +60,8 @@ export default Vue.extend({
       const checkInDate: string = this.$route.query.checkInDate as string
       const checkOutDate: string = this.$route.query.checkOutDate as string
 
+      this.form = { ...this.$route.query }
+
       return {
         hotelName: hotelName ? `Hotel ${hotelName}` : '',
         adults,
@@ -52,8 +69,34 @@ export default Vue.extend({
         checkInDate,
       }
     },
-    openDetails(hotel: any) {
+    openDetails(hotel: any): void {
       this.$router.push({ path: `/${hotel.hotel.hotelId}` })
+    },
+    async startSearch(): Promise<void> {
+      this.isLoading = true
+      const params = (this as any).getParams()
+      const response = await apiService.search(params)
+      const data = await response.json()
+      this.hotels = data.data
+      this.isLoading = false
+    },
+    onSearch(params: any): void {
+      let isValid = true
+      const formData = Object.entries(params)
+      formData.forEach((item) => {
+        if (item[1] === '' || item[1] === 'NaN-NaN-NaN') {
+          isValid = false
+        }
+      })
+      if (!isValid) {
+        this.showToaster = !this.showToaster
+      } else {
+        this.$router.push({ path: `/search`, query: params })
+        this.isLoading = true
+        setTimeout(async () => {
+          await (this as any).startSearch()
+        }, 1000)
+      }
     },
   },
 })
@@ -69,5 +112,11 @@ export default Vue.extend({
   align-items: center;
   justify-content: center;
   margin-top: 50px;
+}
+.tui-hotels-list__not-exists {
+  text-align: center;
+  color: #092a5e;
+  margin-top: 50px;
+  font-size: 2rem;
 }
 </style>
