637dc431c5fe0cb9ddf025d31d592427f74186c5
Eronbello <eron.bello@gmail.com>
2021-04-11 12:29:22 -0300

    feat: add search card

--- components/shared/SearchCard.vue
+++ components/shared/SearchCard.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="tui-search-card">
     <h3 class="tui-search-card__title">Encontre sua estadia perfeita!</h3>
-    <Input type="text" placeholder="Pesquise pelo nome do hotel" />
-    <client-only>
-      <datepicker :highlighted="highlighted"></datepicker>
-    </client-only>
-    <Input type="number" />
-    <a class="tui-search-card__action" href="">Pesquisar</a>
+    <Input
+      v-model="form.hotelName"
+      type="text"
+      placeholder="Pesquise pelo nome do hotel"
+    />
+    <div class="tui-search-card__date">
+      <datepicker
+        v-model="form.checkIn"
+        class="tui-search-card__date-picker"
+        :format="format"
+        placeholder="CHECK-IN"
+      ></datepicker>
+      <datepicker
+        v-model="form.checkOut"
+        :format="format"
+        class="tui-search-card__date-picker"
+        placeholder="CHECK-OUT"
+      ></datepicker>
+    </div>
+    <Input
+      v-model="form.adults"
+      type="number"
+      placeholder="Quantidade de hóspedes"
+    />
+    <NuxtLink
+      class="tui-search-card__action"
+      :to="{
+        path: 'search',
+        query: {
+          hotelName: form.hotelName,
+          adults: form.adults,
+          checkInDate: formatDate(form.checkIn),
+          checkOutDate: formatDate(form.checkOut),
+        },
+      }"
+      >Pesquisar</NuxtLink
+    >
   </div>
 </template>
 
@@ -14,14 +45,23 @@
 import Vue from 'vue'
 import Input from '@/components/common/Input.vue'
 import Datepicker from 'vuejs-datepicker'
+import dateHelper from '@/helpers/date'
 export default Vue.extend({
   components: { Input, Datepicker },
   data: () => ({
-    highlighted: {
-      to: '2021-04-06T23:51:00.000Z',
-      from: '2021-04-05T23:51:00.000Z',
+    format: 'yyyy-MM-dd',
+    form: {
+      hotelName: '',
+      checkIn: '',
+      checkOut: '',
+      adults: '',
     },
   }),
+  methods: {
+    formatDate(date: Date) {
+      return dateHelper(date)
+    },
+  },
 })
 </script>
 <style lang="scss" scoped>
@@ -29,11 +69,20 @@ export default Vue.extend({
   padding: 40px;
   background-color: white;
   width: 100%;
+  z-index: 200;
   height: 350px;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  &__date {
+    display: flex;
+    justify-content: space-between;
+  }
+  &__date-picker {
+    width: 49%;
+    z-index: 10;
+  }
   &__title {
     font-style: normal;
     font-size: 25px;
