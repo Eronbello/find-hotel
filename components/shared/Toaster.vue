e52213718f4687cd3e555fdee113f55627e9ddd5
Eronbello <eron.bello@gmail.com>
2021-04-11 16:33:43 -0300

    feat: add toaster

---
+++ components/shared/Toaster.vue
@@ -0,0 +1,109 @@
+<template>
+  <div
+    :style="{ backgroundColor: color }"
+    :class="['toaster', showToaster && 'toaster--show']"
+  >
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    message: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    color: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+  data: () => ({
+    showToaster: false,
+  }),
+  watch: {
+    show() {
+      this.showToaster = this.show
+      setTimeout(() => {
+        this.showToaster = false
+      }, 2000)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.toaster {
+  visibility: hidden;
+  min-width: 250px;
+  margin-left: -125px;
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  border-radius: 2px;
+  padding: 16px;
+  position: absolute;
+  z-index: 1;
+  right: 0;
+  top: 30px;
+}
+
+.toaster--show {
+  visibility: visible;
+  -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  animation: fadein 0.5s, fadeout 0.5s 2.5s;
+}
+
+@-webkit-keyframes fadein {
+  from {
+    left: 0;
+    opacity: 0;
+  }
+  to {
+    right: 30px;
+    opacity: 1;
+  }
+}
+
+@keyframes fadein {
+  from {
+    right: 0;
+    opacity: 0;
+  }
+  to {
+    right: 30px;
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes fadeout {
+  from {
+    right: 30px;
+    opacity: 1;
+  }
+  to {
+    right: 0;
+    opacity: 0;
+  }
+}
+
+@keyframes fadeout {
+  from {
+    right: 30px;
+    opacity: 1;
+  }
+  to {
+    right: 0;
+    opacity: 0;
+  }
+}
+</style>
